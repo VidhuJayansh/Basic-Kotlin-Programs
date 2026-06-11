33949f8d5fa3e687c466a9f6aa58c53f1e2b0327
Vidhu Jayansh <20.jayanshvidhu@gmail.com>
2026-06-11 23:36:33 +0530

    Q16.kt

---
+++ Q16.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    // Celsius to Fahrenheit
+    val celsius = 30
+    val fahrenheit = (celsius * 9/5) + 32
+
+    println("Fahrenheit = $fahrenheit")
+
+    // Fahrenheit to Celsius
+    val fah = 86
+    val cel = (fah - 32) * 5/9
+
+    println("Celsius = $cel")
+
+}
