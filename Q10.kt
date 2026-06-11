0495aefbcc5c11803ad40b208a10fb7f12e6ab08
Vidhu Jayansh <20.jayanshvidhu@gmail.com>
2026-06-11 23:29:41 +0530

    Q10.kt

---
+++ Q10.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val ch = 'a'
+
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+        println("Vowel")
+    }
+    else{
+        println("Consonant")
+    }
+
+}
