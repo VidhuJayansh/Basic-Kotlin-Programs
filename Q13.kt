e47f698dd2443f05ab5038a8de22e2c4699b5ffc
Vidhu Jayansh <20.jayanshvidhu@gmail.com>
2026-06-11 23:34:05 +0530

    Q13.kt

---
+++ Q13.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val principal = 1000
+    val rate = 5
+    val time = 2
+
+    val simpleInterest = (principal * rate * time) / 100
+
+    println("Simple Interest = $simpleInterest")
+
+}
