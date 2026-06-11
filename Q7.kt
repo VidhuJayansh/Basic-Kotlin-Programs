c16458c0c6fa9bb72250a02134209751d5cc3c96
Vidhu Jayansh <20.jayanshvidhu@gmail.com>
2026-06-11 23:27:08 +0530

    Q7.kt

---
+++ Q7.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val a = 10
+    val b = 20
+    val c = 15
+
+    if(a >= b && a >= c){
+        println("$a is largest")
+    }
+    else if(b >= a && b >= c){
+        println("$b is largest")
+    }
+    else{
+        println("$c is largest")
+    }
+
+}
